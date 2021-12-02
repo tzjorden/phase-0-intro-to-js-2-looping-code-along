b3ecc880d3f26b0d174136924ce67ed56b23b610
Tenzin Jorden <tenden96@gmail.com>
2021-12-01 23:32:46 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+const name = (["Lisa", "Kaitlin", "Jan" ]);
+const messages = [];
+
+function writeCards(name,event){
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        debugger;
+      }
+
+      return messages;
+    
+}
+
+function countDown(numbers) {
+    let i = 10;
+    while(i >= 0){
+        console.log(i);
+        i--;
+
+    } 
+    return numbers;
+}
